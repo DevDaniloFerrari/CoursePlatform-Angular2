db15c33e731ea81cde7d680f7bdedb375a0bb33f
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-07-11 13:10:52 -0300

    inicializando a aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { ListCoursesComponent } from './components/list-courses/list-courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    HomeComponent,
+    CourseDetailsComponent,
+    ListCoursesComponent
   ],
   imports: [
     BrowserModule,
